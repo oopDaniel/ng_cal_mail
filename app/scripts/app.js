779af7f9b2c5988725a89a85fa8925921153c530
DanielChiang <ssw1200@gmail.com>
2016-01-31 23:09:07 +0800

    it should have shown

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,22 +10,52 @@
  */
 angular
   .module('calculatorApp', [
+    'ui.router',
     'ngResource',
     'ngRoute'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('app', {
+        url:'/',
+        views: {
+          'header': {
+            templateUrl: 'views/header.html'
+          },
+          'content': {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+          },
+          'footer': {
+            templateUrl: 'views/footer.html'
+          }
+        }
       })
-      .when('/project', {
-        templateUrl: 'views/project.html',
-        controller: 'ProjectCtrl',
-        controllerAs: 'project'
-      })
-      .otherwise({
-        redirectTo: '/'
+
+      .state('app.project', {
+        url:'project',
+        views: {
+          'content@': {
+            templateUrl: 'views/project.html',
+            controller: 'ProjectCtrl'
+          }
+        }
       });
+      $urlRouterProvider.otherwise('/');
   });
+  // .config(function ($routeProvider) {
+  //   $routeProvider
+  //     .when('/', {
+  //       templateUrl: 'views/main.html',
+  //       controller: 'MainCtrl',
+  //       controllerAs: 'main'
+  //     })
+  //     .when('/project', {
+  //       templateUrl: 'views/project.html',
+  //       controller: 'ProjectCtrl',
+  //       controllerAs: 'project'
+  //     })
+  //     .otherwise({
+  //       redirectTo: '/'
+  //     });
+  // });
