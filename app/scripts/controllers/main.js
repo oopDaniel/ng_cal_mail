1773e46c1790b589b79fe76eadcd7b0b6cd2f706
DanielChiang <ssw1200@gmail.com>
2016-02-01 18:22:38 +0800

    start binding the data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,25 @@
  */
 angular.module('calculatorApp')
     .controller('MainCtrl', [ '$scope', function ($scope) {
-        $scope.onNVR = true;
+        //$scope.onNVR = true;
+        $scope.NVRObj={
+          itemName:'',
+          storage:0,
+          bandwidth:0,
+          cameras:16,
+          bitRate:4,
+          bitRateData: {
+            codec:'H.264',
+            quality:'M',
+            resolution:'1920x1080',
+            FPS:30
+            },
+          rDays:30,
+          motion:50,
+          RAID:5,
+          HDDsize:3
+        };
+
 
 
 
