f476c2618b0beb9028347b732240132214cdf6c9
DanielChiang <ssw1200@gmail.com>
2016-02-02 02:07:36 +0800

    working on range for motion recording

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -31,10 +31,38 @@ angular.module('calculatorApp')
         // for checking whether should the class be applied
         $scope.setRAID = new Array(4);
 
-      $scope.selectRAID = function(index) {
-        $scope.setRAID.fill(false);
-        $scope.setRAID[index] = true;
-      };
+        // click a type of RAID
+        $scope.selectRAID = function(index) {
+            $scope.setRAID.fill(false);
+            $scope.setRAID[index] = true;
+        };
+
+
+        $scope.rDaysArr = [
+            {name:'1 day'},
+            {name:'2 days'},
+            {name:'4 days'},
+            {name:'7 days (1 week)'},
+            {name:'14 days (2 weeks)'},
+            {name:'30 days (1 month)'},
+            {name:'60 days (2 months)'},
+            {name:'90 days (3 months)'},
+            {name:'Other duration'}
+        ];
+        // set the default value for the combo box
+        $scope.rDays = {name:'30 days (1 month)'};
+        // display the textarea for other duration
+        $scope.otherDuration = false;
+        //
+        $scope.selectDays = function() {
+            var tmp = parseInt($scope.rDays.name);
+            if ( isNaN(tmp) ) {
+                $scope.otherDuration = true;
+            } else {
+                $scope.otherDuration = false;
+                $scope.NVRObj.rDays = tmp;
+            }
+        };
 
 
 
