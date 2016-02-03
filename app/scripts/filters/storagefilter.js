402ea55a0c7bfcd14b7d1a0d499e1a5f97fb3c8b
DanielChiang <ssw1200@gmail.com>
2016-02-03 18:35:37 +0800

    alter the filter

--- app/scripts/filters/storagefilter.js
+++ app/scripts/filters/storagefilter.js
@@ -17,9 +17,13 @@ angular.module('calculatorApp')
                 return $filter('number')(input / 1024 / 1024, 1);
             else if ( input > 10240 )
                 return $filter('number')(input / 1024, 1);
-            else
-                return input === (input|0) ? // filter the decimal numbers
-                    $filter('number')(input) :
-                    $filter('number')(input, 1);
+            else if ( input === (input|0) )
+                return $filter('number')(input);
+            else {
+                var tmpNum = $filter('number')(input, 2);
+                var tmpStr = tmpNum.toString();
+                var s = tmpStr.slice( tmpStr.length - 1, tmpStr.length);
+                return s === '0' ? $filter('number')(input, 1) : tmpNum;
+            }
         };
   }]);
