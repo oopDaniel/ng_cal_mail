2d01c9015e43aff34892e69ed60b76d8e3a75ff1
Daniel <ssw1200@gmail.com>
2016-02-26 11:03:00 +0800

    comment 'karma'

--- Gruntfile.js
+++ Gruntfile.js
@@ -503,8 +503,9 @@ module.exports = function (grunt) {
     'wiredep',
     'concurrent:test',
     'postcss',
-    'connect:test',
-    'karma'
+    'connect:test'
+    // ,
+    // 'karma'
   ]);
 
   grunt.registerTask('build', [
