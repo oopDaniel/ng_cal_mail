f4bab947611232b7eb2a6e00024ba52345dcc1c7
DanielChiang <ssw1200@gmail.com>
2016-02-29 13:16:10 +0800

    file format

--- app/scripts/services/fileProcessService.js
+++ app/scripts/services/fileProcessService.js
@@ -4,34 +4,34 @@ angular.module('calculatorApp')
   .service('fileProcessService', ['$window', function($window) {
 
     var title = [
-      "Project ID",
-      "Project Name",
-      "Total Storage",
-      "Total Bandwidth",
-      "NVR Count",
-      "CMS Count",
-      "Item ID",
-      "Item Name",
-      "Type",
-      "Storage",
-      "Bandwidth",
-      "Cameras",
-      "Bit Rate",
-      "Codec",
-      "Quality",
-      "Resolution",
-      "FPS",
-      "Estimated Days",
-      "Recording Days",
-      "Recording Hours",
-      "Motion(%)",
-      "RAID Type",
-      "HDD Size",
-      "Local User",
-      "Remote Users"
+      'Project ID',
+      'Project Name',
+      'Total Storage',
+      'Total Bandwidth',
+      'NVR Count',
+      'CMS Count',
+      'Item ID',
+      'Item Name',
+      'Type',
+      'Storage',
+      'Bandwidth',
+      'Cameras',
+      'Bit Rate',
+      'Codec',
+      'Quality',
+      'Resolution',
+      'FPS',
+      'Estimated Days',
+      'Recording Days',
+      'Recording Hours',
+      'Motion(%)',
+      'RAID Type',
+      'HDD Size',
+      'Local User',
+      'Remote Users'
     ];
 
-    var titleStr  = title.join() + "\n";
+    var titleStr  = title.join() + '\n';
 
 
 
@@ -63,41 +63,49 @@ angular.module('calculatorApp')
         var obj = loadData ();
         var tmp = '';
         for ( var i in obj ) {
-            tmp     += obj[i]._id       + "," +
-                       obj[i].name      + "," +
-                       obj[i].storage   + "," +
-                       obj[i].bandwidth;
-            tmp     += addComma(19) + "\n" + addComma(6);
+            if ( i > 0 ) {
+                tmp += '\n';
+            }
+            tmp     += obj[i]._id       + ',' +
+                       obj[i].name      + ',' +
+                       obj[i].storage   + ',' +
+                       obj[i].bandwidth + ',' +
+                       obj[i].count.NVR + ',' +
+                       obj[i].count.CMS;
+            tmp     += addComma(19) + '\n' + addComma(6);
             for ( var j in obj[i].data ) {
+                if ( j > 0 ) {
+                    tmp += addComma(6);
+                }
                 var inf     = obj[i].data[j];
                 var type    = inf.type;
                 var data    = inf.data;
                 var bitRate = data.bitRate;
                 var estDays = data.estDays;
-                tmp        += inf._id                   + "," +
-                              inf.name                  + "," +
-                              type                      + "," +
-                              data.display.storage      + "," +
-                              data.display.bandwidth    + "," +
-                              data.cameras              + "," +
-                              bitRate.data              + "," +
-                              bitRate.params.codec      + "," +
-                              bitRate.params.quality    + "," +
-                              bitRate.params.resolution + "," +
-                              bitRate.params.FPS        + "," ;
+                tmp        += inf._id                   + ',' +
+                              inf.name                  + ',' +
+                              type                      + ',' +
+                              data.display.storage      + ',' +
+                              data.display.bandwidth    + ',' +
+                              data.cameras              + ',' +
+                              bitRate.data              + ',' +
+                              bitRate.params.codec      + ',' +
+                              bitRate.params.quality    + ',' +
+                              bitRate.params.resolution + ',' +
+                              bitRate.params.FPS        + ',' ;
 
-                tmp     += "NVR" === type ?
-                              estDays.data              + "," +
-                              estDays.params.rDays      + "," +
-                              estDays.params.rHours     + "," +
-                              estDays.params.motion     + "," +
-                              data.RAID                 + "," +
-                              data.HDDsize              + "," +
+                tmp     += 'NVR' === type ?
+                              estDays.data              + ',' +
+                              estDays.params.rDays      + ',' +
+                              estDays.params.rHours     + ',' +
+                              estDays.params.motion     + ',' +
+                              data.RAID                 + ',' +
+                              data.HDDsize              + ',' +
                               data.local              :
                               addComma(6)               +
-                              data.local                + "," +
+                              data.local                + ',' +
                               data.remoteUsers;
-                tmp     += "\n";
+                tmp     += '\n';
             }
         }
         return tmp;
@@ -112,8 +120,8 @@ angular.module('calculatorApp')
 
     this.saveFile = function() {
         var fileStr = titleStr + storedData2Str(),
-            file    = new Blob([fileStr], {type: "text/plain;charset=utf-8"});
-        saveAs(file, "data.csv");
+            file    = new Blob([fileStr], {type: 'text/plain;charset=utf-8'});
+        saveAs(file, 'data.csv');
     };
 
 
