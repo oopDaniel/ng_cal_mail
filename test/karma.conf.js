b5bed7b91e52e9509c48f57a3fad8a8e812a1c05
DanielChiang <ssw1200@gmail.com>
2016-02-29 13:55:19 +0800

    stupid karma error

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function(config) {
       'app/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
-      "test/mock/**/*.js",
+      // "test/mock/**/*.js",
       "test/spec/**/*.js"
     ],
 
