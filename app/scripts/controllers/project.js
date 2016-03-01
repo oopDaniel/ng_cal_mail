895354d95d24ef80073f67d19e6127d70b38568c
Daniel <ssw1200@gmail.com>
2016-03-01 18:38:17 +0800

    it works

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -341,3 +341,43 @@ myApp.controller('ProjectDetailCtrl', ['$scope', '$state', '$stateParams', '$uib
 
     }
 ]);
+
+
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                   Email Controller
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+myApp.controller('MailCtrl', [
+    '$scope',
+    '$cordovaEmailComposer',
+    function(
+        $scope,
+        $cordovaEmailComposer) {
+
+    var email = {
+        to: 'daniel.chiang@isapsolution.com',
+        cc: '',
+        bcc: [''],
+        attachments: [
+            'file://images/yeoman.png'
+        ],
+        subject: 'Cordova Email Test',
+        body: 'yo'
+    };
+
+    document.addEventListener('deviceready', function() {
+
+        $cordovaEmailComposer.isAvailable().then(function() {
+            $scope.sendmail = function() {
+                $cordovaEmailComposer.open(email).then(null, function() {
+                });
+            };
+        }, function() {
+            window.alert('Email is not available now!');
+        });
+    }, false);
+
+}]);
