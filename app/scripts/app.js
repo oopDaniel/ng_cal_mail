10f2bad611d1c86f6bdd59957dc6ede1e0c7a208
Daniel <ssw1200@gmail.com>
2016-03-02 16:19:51 +0800

    removed ng-app in index.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,39 +14,59 @@
 
 //------------- Cordova event setting --------------
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
+angular.element(document).ready(function () {
+   if (window.cordova) {
+     console.log("Running in Cordova, will bootstrap AngularJS once 'deviceready' event fires.");
+     document.addEventListener('deviceready', function () {
+       console.log('Deviceready event has fired, bootstrapping AngularJS.');
+       angular.bootstrap(document.body, ['calculatorApp']);
+     }, false);
+   } else {
+     console.log('Running in browser, bootstrapping AngularJS now.');
+     angular.bootstrap(document.body, ['calculatorApp']);
+   }
+ });
 
-app.initialize();
+
+
+
+
+// var app = {
+//     // Application Constructor
+//     initialize: function() {
+//         this.bindEvents();
+//     },
+//     // Bind Event Listeners
+//     //
+//     // Bind any events that are required on startup. Common events are:
+//     // 'load', 'deviceready', 'offline', and 'online'.
+//     bindEvents: function() {
+//         document.addEventListener('deviceready', this.onDeviceReady, false);
+//     },
+//     // deviceready Event Handler
+//     //
+//     // The scope of 'this' is the event. In order to call the 'receivedEvent'
+//     // function, we must explicitly call 'app.receivedEvent(...);'
+//     onDeviceReady: function() {
+//         app.receivedEvent('deviceready');
+//         angular.bootstrap(domElement, ["angularAppName"])
+//     },
+//     // Update DOM on a Received Event
+//     receivedEvent: function(id) {
+//         var parentElement = document.getElementById(id);
+//         var listeningElement = parentElement.querySelector('.listening');
+//         var receivedElement = parentElement.querySelector('.received');
+
+//         listeningElement.setAttribute('style', 'display:none;');
+//         receivedElement.setAttribute('style', 'display:block;');
+
+//         console.log('Received Event: ' + id);
+//     }
+// };
+
+// app.initialize();
 
 //--------- .end Cordova event setting ----------
 
@@ -59,76 +79,5 @@ angular
         'ngAnimate',
         'ui.bootstrap',
         'ngCordova'
-    ])
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('app', {
-                url:'/',
-                views: {
-                    'header': {
-                        templateUrl: 'views/header.html',
-                        controller: 'HeaderCtrl'
-                    },
-                    'content': {
-                        templateUrl: 'views/main.html',
-                        controller: 'MainCtrl',
-                        resolve: {
-                            onNVR: function() {
-                                return true;
-                            }
-                        }
-                    },
-                    'footer': {
-                        templateUrl: 'views/footer.html'
-                    }
-                }
-            })
+    ]);
 
-            .state('app.cms', {
-                url:'cms',
-                views: {
-                    'content@': {
-                        templateUrl: 'views/main.html',
-                        controller: 'MainCtrl',
-                        resolve: {
-                           onNVR: function() {
-                                return false;
-                            }
-                        }
-                    }
-                }
-            })
-
-            .state('app.project', {
-                url:'project',
-                views: {
-                    'content@': {
-                        templateUrl: 'views/project.html',
-                        controller: 'ProjectCtrl'
-                    }
-                }
-            })
-
-            .state('app.projectdetails', {
-                url:'project/:id',
-                views: {
-                    'content@': {
-                        templateUrl: 'views/projectDetail.html',
-                        controller: 'ProjectDetailCtrl'
-                    }
-                }
-            })
-
-            .state('app.item', {
-                url:'project/:id/:itemid',
-                views: {
-                    'content@': {
-                        templateUrl: 'views/item.html',
-                        controller: 'ItemCtrl'
-                    }
-                }
-            })
-
-      ;
-      $urlRouterProvider.otherwise('/');
-  });
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -71,5 +71,5 @@ angular.module('calculatorApp')
         })
 
         ;
-        $urlRouterProvider.otherwise('/project');
+        $urlRouterProvider.otherwise('/');
     });
