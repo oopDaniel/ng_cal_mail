ba1825258e13cb465568c334293723a75057ae92
Daniel <ssw1200@gmail.com>
2016-03-03 16:54:54 +0800

    yo

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,21 +20,14 @@ angular.element(document).ready(function () {
    if (window.cordova) {
      console.log("Running in Cordova, will bootstrap AngularJS once 'deviceready' event fires.");
      document.addEventListener('deviceready', function () {
+
        console.log('Deviceready event has fired, bootstrapping AngularJS.');
        angular.bootstrap(document.body, ['MyApp']);
 
-
-
-
        // ---------- Ugly code start ---------
-
        // document.getElementsByTagName('header')[0].setAttribute('ng-controller','HeaderCtrl');
        // document.getElementById('header').setAttribute('ng-controller','HeaderCtrl');
-
        // ---------- Ends ugly code ----------
-
-
-
 
      }, false);
    } else {
